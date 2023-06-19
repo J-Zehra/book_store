7f1414e6d5714c3b57641e9d4ec3b7f3d5710ed7
Jad <jazencode@gmail.com>
2023-06-19 16:56:18 +0800

    added preload

--- utils/font.ts
+++ utils/font.ts
@@ -3,18 +3,18 @@ import { Open_Sans, Amatic_SC, Dancing_Script } from "next/font/google";
 export const open_sans = Open_Sans({
   subsets: ["latin"],
   display: "swap",
-  preload: true,
+  preload: false,
 });
 
 export const amatic_sc = Amatic_SC({
   subsets: ["latin"],
   display: "swap",
   weight: "700",
-  preload: true,
+  preload: false,
 });
 
 export const dancing_script = Dancing_Script({
   subsets: ["latin"],
   display: "swap",
-  preload: true,
+  preload: false,
 });
