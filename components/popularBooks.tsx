b94a1eba35acc953446dee9d801fca46fc46f2ac
Jad <jazencode@gmail.com>
2023-06-21 19:43:40 +0800

    working on popular books

--- components/popularBooks.tsx
+++ components/popularBooks.tsx
@@ -1,13 +1,37 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 
 export default function PopularBooks() {
   return (
-    <Box bgcolor="#E9EAEB" sx={{ paddingBlock: "20rem" }}>
+    <Box bgcolor="#E9EAEB" sx={{ paddingBlock: "4rem" }}>
       <Container maxWidth="lg">
-        {/* <Typography fontSize="1.2rem" fontWeight="bold">
-          Top 7 Popular
-        </Typography> */}
+        <Stack direction="row" spacing={1}>
+          <Typography fontSize="1.1rem" fontWeight="bold">
+            TOP 7 POPULAR
+          </Typography>
+          <Typography fontSize="1.1rem" fontWeight="bold" color="primary.500">
+            BOOKS
+          </Typography>
+        </Stack>
+        <Stack
+          justifyContent="space-between"
+          spacing={2}
+          direction="row"
+          marginTop="2rem"
+        >
+          <Box
+            bgcolor="rgba(0, 0, 0, .1)"
+            borderRadius=".3rem"
+            width="100%"
+            height="20rem"
+          ></Box>
+          <Box
+            bgcolor="rgba(0, 0, 0, .1)"
+            borderRadius=".3rem"
+            width="100%"
+            height="20rem"
+          ></Box>
+        </Stack>
       </Container>
     </Box>
   );
