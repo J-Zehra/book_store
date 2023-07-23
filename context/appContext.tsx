49feb5470713e8148b2c87a427da352fcf15aba8
Jad <jazencode@gmail.com>
2023-07-23 10:27:55 +0800

    refined

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import React, { createContext, useMemo, useState } from "react";
-
-export const ApplicationContext = createContext<AppContextValues | null>(null);
+import { RecoilRoot } from "recoil";
 
 export default function AppContext({ children }: ChildrenProp) {
-  const [activeNav, setActiveNav] = useState<string>("Home");
-
-  const values = useMemo(() => {
-    const items: AppContextValues = {
-      activeNav,
-      setActiveNav,
-    };
-    return items;
-  }, [activeNav]);
-
-  return (
-    <ApplicationContext.Provider value={values}>{children}</ApplicationContext.Provider>
-  );
+  return <RecoilRoot>{children}</RecoilRoot>;
 }
